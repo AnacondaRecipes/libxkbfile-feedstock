6b94a443c1e80124479eabdd531e0f1e6f6b6235
Andrii Vetroumov <avetroumov@anaconda.com>
2026-08-20 08:53:58 +0300

    PKG-16232: libxkbfile 1.2.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,14 @@
 set -ex
 
 # Get an updated config.sub and config.guess
-chmod +w ./config.guess ./config.sub
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./
+# chmod +w ./config.guess ./config.sub
+# cp $BUILD_PREFIX/share/gnuconfig/config.* ./
 
-autoreconf -ivf
+# autoreconf -ivf
 
-./configure --prefix=$PREFIX
-make
-make install
-make check
+# ./configure --prefix=$PREFIX
+# make
+# make install
+# make check
+meson setup builddir --prefix=${PREFIX}
+meson compile -C builddir
