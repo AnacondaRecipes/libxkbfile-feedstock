c5e2d5643e32ae3f1622551531dac5b1333b555e
Andrii Vetroumov <avetroumov@anaconda.com>
2026-08-20 09:19:52 +0300

    Fix tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-# Get an updated config.sub and config.guess
-# chmod +w ./config.guess ./config.sub
-# cp $BUILD_PREFIX/share/gnuconfig/config.* ./
-
-# autoreconf -ivf
-
-# ./configure --prefix=$PREFIX
-# make
-# make install
-# make check
 meson setup builddir --prefix=${PREFIX}
 meson compile -C builddir
 meson install -C builddir
